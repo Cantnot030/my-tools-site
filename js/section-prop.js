76ea25a38357093a38fc4d3a2bf54a95eee47610
Cantnot030 <masaphantom@gmail.com>
2026-03-12 18:28:55 +0900

    section-prop.js を作成

---
+++ js/section-prop.js
@@ -0,0 +1,62 @@
+function calculate(){
+
+let shape = document.getElementById("shape").value;
+
+if(shape==="rect"){
+rectCalc();
+}
+
+if(shape==="circle"){
+circleCalc();
+}
+
+if(shape==="pipe"){
+pipeCalc();
+}
+
+if(shape==="hbeam"){
+hbeamCalc();
+}
+
+function rectCalc(){
+
+let b = parseFloat(document.getElementById("b").value);
+let h = parseFloat(document.getElementById("h").value);
+
+let A = b*h;
+let I = b*Math.pow(h,3)/12;
+let Z = b*Math.pow(h,2)/6;
+
+document.getElementById("result").innerHTML =
+"A="+A+"<br>I="+I+"<br>Z="+Z;
+
+}
+
+function circleCalc(){
+
+let d = parseFloat(document.getElementById("d").value);
+
+let A = Math.PI*d*d/4;
+let I = Math.PI*Math.pow(d,4)/64;
+let Z = Math.PI*Math.pow(d,3)/32;
+
+document.getElementById("result").innerHTML =
+"A="+A+"<br>I="+I+"<br>Z="+Z;
+
+}
+
+function pipeCalc(){
+
+let D = parseFloat(document.getElementById("D").value);
+let d = parseFloat(document.getElementById("d").value);
+
+let A = Math.PI*(D*D-d*d)/4;
+let I = Math.PI*(Math.pow(D,4)-Math.pow(d,4))/64;
+let Z = I/(D/2);
+
+document.getElementById("result").innerHTML =
+"A="+A+"<br>I="+I+"<br>Z="+Z;
+
+}
+
+}
